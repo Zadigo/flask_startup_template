77b4c3990f44c2ca5aaa47e4782735b8e49a9e34
John <pendenquejohn@gmail.com>
2019-06-03 16:50:59 +0200

    First commit

---
+++ karista.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask
+from flask import render_template
+
+BASE_DIR = os.path.basename(os.path.dirname(__file__))
+
+app = Flask(__name__)
+
+app.config['SECRET KEY'] = ''
+app.config['MEDIA'] = os.path.join(BASE_DIR, 'media')
+
+@app.route('/', methods=['GET'])
+def hero():
+    print(app.config['MEDIA'])
+    return render_template('home/hero.htm')
+
+print(BASE_DIR)
+
+if __name__ == "__main__":
+    app.run(port='8000', debug=True)
