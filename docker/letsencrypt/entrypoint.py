2b0a940aeb8d15053d7ce49cf5c649654b350033
John <pendenquejohn@gmail.com>
2019-06-04 20:17:43 +0200

    Certbot

---
+++ docker/letsencrypt/entrypoint.py
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+import posixpath
+
+BASE_PATH = '/data/certbot'
+
+class CreateCertbot:
+    """This class creates all the necessary files
+    for CERTBOT (HTTPS).
+    """
+    def __init__(self):
+        # Check if the path exists
+        # in the container -- else,
+        # we have to create it
+        if self.data_dir_exists:
+            pass
+
+    @property
+    def data_dir_exists(self):
+        if posixpath.exists(BASE_PATH):
+            return True
+        return False
+
+CreateCertbot()
