29eb4c29ef57c7fe8ff0041d980c03148549b0e2
John <pendenquejohn@gmail.com>
2019-06-04 21:01:11 +0200

    Test

--- docker/letsencrypt/init-letsencrypt.sh
+++ docker/letsencrypt/init-letsencrypt.sh
@@ -6,4 +6,5 @@ data_path="./data/certbot"
 # configuration files to start CERTBOT
 if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
   echo "1. Downloading recommended TLS parameters"
+  echo
 fi
